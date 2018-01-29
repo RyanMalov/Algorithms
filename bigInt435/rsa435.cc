6d0bae15a1078d81564aef8ec6fa681ece780c3f
RyanMalov <ram179@zips.uakron.edu>
2018-01-29 14:30:16 -0500

    Cleaned up code, added solving for n.

--- bigInt435/rsa435.cc
+++ bigInt435/rsa435.cc
@@ -14,28 +14,39 @@ int main() {
 	 * wrong.  It's a good idea to catch them using a `try' block like this
 	 * one.  Your C++ compiler might need a command-line option to compile
 	 * code that uses exceptions. */
-	try {
+	try 
+	{
 		      
       std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
-      BigInteger big1 = BigInteger(1);
-      for (int i=0;i<400;i++) {
-         big1 = big1*10 +rand();
+      BigInteger p = BigInteger(1);
+	  BigInteger q = BigInteger(1);
+	  BigInteger n = BigInteger(1);
+
+	  //Setup the seeding for randomization
+	  srand(time(NULL));
+
+	  //Create the p and q for the RSA
+      for (int i=0; i<278; i++) 
+	  {
+         p = p*10 + (rand() % 10);
+		 q = q*10 + (rand() % 10);
       }
-      std::cout << "my big1 !!!\n";
-      std::cout << big1 << std::endl << std::endl;
-      BigInteger big2 = BigInteger(1);
-      for (int i=0;i<400;i++) {
-         big2 = big2*10 +rand();
-      }
-      std::cout << "my big2 !!!\n";
-      std::cout << big2 << std::endl << std::endl;
-      std::cout << "my big3 = big1*big2 !!!\n";
-      BigInteger big3 = big1*big2;
-      std::cout <<big3 << std::endl << std::endl;
-      std::cout << "my big3/big2 !!!\n";
-      std::cout <<big3/big2 << std::endl << std::endl;
+
+
+	  //solve for n
+	  n = p * q;
+
+	  //Output p, q and n
+      std::cout << "p =\n";
+	  std::cout << p << std::endl;
+	  std::cout << "q = " << std::endl;
+	  std::cout << q << std::endl;
+	  std::cout << "n = " << std::endl;
+	  std::cout << n << std::endl;
       
-	} catch(char const* err) {
+	} 
+	catch(char const* err) 
+	{
 		std::cout << "The library threw an exception:\n"
 			<< err << std::endl;
 	}
