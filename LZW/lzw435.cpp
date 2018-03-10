637e6f54ced8cf869bc55ec364510c8cd3e7ae1b
RyanMalov <ram179@zips.uakron.edu>
2018-03-10 13:12:48 -0500

    Started working on Main in LZW project

--- LZW/lzw435.cpp
+++ LZW/lzw435.cpp
@@ -1,3 +1,6 @@
+//Ryan Malov
+//Algorithms Project 2 LZW
+
 #include <string>
 #include <map>
 #include <iostream>
@@ -20,15 +23,20 @@ Iterator compress(const std::string &uncompressed, Iterator result) {
   std::map<std::string,int> dictionary;
   for (int i = 0; i < 256; i++)
     dictionary[std::string(1, i)] = i;
- 
+
+	//Extended ASCII Table creation here 
   std::string w;
-  for (std::string::const_iterator it = uncompressed.begin();
-       it != uncompressed.end(); ++it) {
+  for (std::string::const_iterator it = uncompressed.begin(); it != uncompressed.end(); ++it) 
+  {
     char c = *it;
     std::string wc = w + c;
+
     if (dictionary.count(wc))
+	{
       w = wc;
-    else {
+	}
+    else 
+	{
       *result++ = dictionary[w];
       // Add wc to the dictionary. Assuming the size is 4096!!!
       if (dictionary.size()<4096)
@@ -39,7 +47,9 @@ Iterator compress(const std::string &uncompressed, Iterator result) {
  
   // Output the code for w.
   if (!w.empty())
+  {
     *result++ = dictionary[w];
+  }
   return result;
 }
  
@@ -51,18 +61,27 @@ std::string decompress(Iterator begin, Iterator end) {
   int dictSize = 256;
   std::map<int,std::string> dictionary;
   for (int i = 0; i < 256; i++)
+  {
     dictionary[i] = std::string(1, i);
- 
+  }
+
   std::string w(1, *begin++);
   std::string result = w;
-  std::cout << result<<"???:::\n";
+//  std::cout << result<<"???:::\n";
   std::string entry;
-  for ( ; begin != end; begin++) {
+
+  for ( ; begin != end; begin++) 
+  {
     int k = *begin;
+
     if (dictionary.count(k))
+	{
       entry = dictionary[k];
+	}
     else if (k == dictSize)
+	{
       entry = w + w[0];
+	}
     else
       throw "Bad compressed k";
  
@@ -70,51 +89,73 @@ std::string decompress(Iterator begin, Iterator end) {
  
     // Add w+entry[0] to the dictionary.
     if (dictionary.size()<4096)
+	{
       dictionary[dictSize++] = w + entry[0];
- 
+	}
+
     w = entry;
   }
+
   return result;
 }
 
 std::string int2BinaryString(int c, int cl) {
       std::string p = ""; //a binary code string with code length = cl
       int code = c;
-      while (c>0) {         
+      
+	  while (c>0) 
+	  {         
 		   if (c%2==0)
+		   {
             p="0"+p;
+		   }
          else
+		 {
             p="1"+p;
+		 }
          c=c>>1;   
       }
       int zeros = cl-p.size();
-      if (zeros<0) {
+      if (zeros<0) 
+	  {
          std::cout << "\nWarning: Overflow. code " << code <<" is too big to be coded by " << cl <<" bits!\n";
          p = p.substr(p.size()-cl);
       }
       else {
-         for (int i=0; i<zeros; i++)  //pad 0s to left of the binary code if needed
+         for (int i=0; i<zeros; i++)
+		 {	 //pad 0s to left of the binary code if needed
             p = "0" + p;
+		 }
       }
       return p;
 }
 
 int binaryString2Int(std::string p) {
    int code = 0;
-   if (p.size()>0) {
-      if (p.at(0)=='1') 
+
+   if (p.size()>0) 
+   {
+      if (p.at(0)=='1')
+	  {
          code = 1;
+	  }
       p = p.substr(1);
-      while (p.size()>0) { 
+
+      while (p.size()>0) 
+	  { 
          code = code << 1; 
 		   if (p.at(0)=='1')
+		   {
             code++;
+		   }
          p = p.substr(1);
       }
    }
    return code;
 }
 
+
+/*
 void binaryIODemo(std::vector<int> compressed) {
    int c = 69;
    int bits = 9;
@@ -187,8 +228,37 @@ void binaryIODemo(std::vector<int> compressed) {
    myfile2.close();
    std::cout << " saved string : "<<s << "\n"; 
 }
- 
-int main() {
+*/ 
+int main(int argc, char* argv[]) 
+{
+	//Throw error if not enough paramteters were  given.
+	if(argc < 2)
+	{
+		std::cerr << "No input file given\n";
+		return 1;
+	}
+
+	std::string filename = argv[2];
+	std::ifstream infile(filename.c_str(), std::ios::binary);
+	
+	//Create a string of the file contents that a read in	
+	std::string input{std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>()};
+	infile.close();
+
+	if(*argv[1] == 'c')
+	{
+		//Avector (or ASCII Vector) takes the input string of the selected
+		//file and stores them as integer values inside of a vector.
+		std::vector<int> Avector = compress(input, std::back_inserter(Avector));
+		copy(Avector.begin(), Avector.end(), std::ostream_iterator<int>(std::cout, ", "));
+		std::cout << std::endl;
+	}
+
+		
+		
+/*
+//This is the example code from the original project.
+		
   std::vector<int> compressed;
   compress("AAAAAAABBBBBB", std::back_inserter(compressed));
   copy(compressed.begin(), compressed.end(), std::ostream_iterator<int>(std::cout, ", "));
@@ -199,4 +269,5 @@ int main() {
   binaryIODemo(compressed);
   
   return 0;
+*/
 }
